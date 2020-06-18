a4d28112bb94cf8882fbbadbec39018ed5c5cad6
Elan Fraga <elanfraga@gmail.com>
2020-06-17 21:02:53 -0300

    Listar os pratos favoritos da sua API

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,16 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get<Food[]>('/favorites');
+
+      const favoritesData = response.data.map((food: Food) => {
+        return {
+          ...food,
+          formattedPrice: formatValue(food.price),
+        };
+      });
+
+      setFavorites(favoritesData);
     }
 
     loadFavorites();
